55102c5f41a888d397d1d59d08bc57c388bdc3a2
shubhamtrn <shubhamkumartrn@gmail.com>
2022-08-18 17:11:05 +0530

    new

---
+++ git_project/src/git_project/main_class.java
@@ -0,0 +1,10 @@
+package git_project;
+
+public class main_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("gittt hubbbb...");
+	}
+
+}
